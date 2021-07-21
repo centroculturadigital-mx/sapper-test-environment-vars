79f454e1d55b97cc37598f660b05503f1ba27d2c
furenku <furenku@gmail.com>
2021-07-20 19:54:50 -0500

    env vars being read on runtime

---
+++ src/server.js
@@ -0,0 +1,29 @@
+import sirv from 'sirv';
+import polka from 'polka';
+import compression from 'compression';
+import * as sapper from '@sapper/server';
+
+const { PORT, NODE_ENV } = process.env;
+const dev = NODE_ENV === 'development';
+
+
+import dotenv from "dotenv";
+const config = dotenv.config();
+
+const { TEST_VAR } = process.env;
+
+
+
+polka() // You can also use Express
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware({
+			session: (req, res) => ({
+			  TEST_VAR: TEST_VAR,
+			}),
+		})
+	)
+	.listen(PORT, err => {
+		if (err) console.log('error', err);
+	});
